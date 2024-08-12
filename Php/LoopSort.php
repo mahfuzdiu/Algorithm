2f5acaf3927e9d3b889550cf00c8650229cc3d4c
mahfuz <mahfuz@dorpd.org>
2024-08-12 15:12:34 +0600

    added simple sort/ bubble sort in php

---
+++ Php/LoopSort.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * ALso Bubble sort
+ * */
+
+class LoopSort
+{
+    function __construct(){
+        $this->sort();
+    }
+
+    public function sort(){
+        $array = [10, 5, 6 , 9, 1 , 7];
+        echo "Unsorted array: " . json_encode($array) . PHP_EOL;
+        //logic: just swap the values
+        //to understand sort [3,2,1] in paper
+        for ($i = 0; $i < count($array); $i++){
+            for ($j = 0; $j < count($array) - 1; $j++){
+                if($array[$j] > $array[$j + 1]){
+                    $swapValue = $array[$j + 1];
+                    $array[$j + 1] = $array[$j];
+                    $array[$j] = $swapValue;
+                }
+            }
+        }
+
+        echo "Sorted array: " . json_encode($array);
+    }
+}
+
+$obj = new LoopSort();
+
+//command to run code in cmd
+//cd > php > php LoopSort.php
